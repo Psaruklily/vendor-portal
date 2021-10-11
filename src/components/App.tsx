acfab32fa5691a6df44e0294a578d23cf63888eb
Psaruklily <liliia.psaruk@pickethomes.com>
2021-10-11 10:02:12 +0300

    add eslint configuration

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,8 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'elara-react-common';
 
-export default function App(){
-    return(
-        <div><Link text='hda' href='#' /></div>
-    )
-}
-
-
-
+export default function App() {
+  return (
+    <div> <Link text="hda" href="https://andrebnassis.medium.com/setting-eslint-on-a-react-typescript-project-2021-1190a43ffba" /> </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,11 @@ import './styles/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
